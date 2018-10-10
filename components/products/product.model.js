5604bab04288f8d28bd92f49404f7f97a6ff9a37
Sunny <sunnyrai31@gmail.com>
2018-10-11 02:40:35 +0530

    cross origin allow

--- components/products/product.model.js
+++ components/products/product.model.js
@@ -8,7 +8,7 @@ var ProductSchema = new Schema({
     price: {type: Number, required: true},
     stock: {type: Number, required: true},
     image: {type: String, required: true},
-    brand:{type: String, required: true}
+    brand: {type: String, required: true}
 })
 module.exports = mongoose.model('Product', ProductSchema);
 
--- index.js
+++ index.js
@@ -6,6 +6,19 @@ var dbConfig  = require('./config');
 var bodyParser = require('body-parser');
 var app = express();
 
+app.use(function(request, response, next) {
+    response.header('Access-Control-Allow-Origin', request.headers.origin);
+    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    // intercept OPTIONS method
+    if ( request.method == 'OPTIONS' ) {
+      response.status(200).send();
+    } else {
+      next();
+    }
+});
+
+
 //configuration for body parser
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: false, parameterLimit:50000}));
